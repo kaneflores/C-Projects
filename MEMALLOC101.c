9830913b9d6420a1f706b5b6f67720bdcbb091c1
kaneflores <kaneflores911@gmail.com>
2026-04-14 16:30:15 -0400

    start of malloc

---
+++ MEMALLOC101.c
@@ -0,0 +1,17 @@
+// Welcome to MEMALLOC 101 project guide
+// sbrk() lets us manipulate the program break ot release/allocate memory in the heap
+///
+// sbrk(0) gives the current address of the pgrm break
+// sbrk(x) with a positive value increments brk by x bytes, as a result of allocating memory
+// sbrk(-x) with a negative value decrements brk by x bytes, as a result of releasing memory
+// on failutre, sbrk() returns (void*) -1.
+
+//MALLOC*****************
+void *malloc(size_t size){
+
+    void *block;
+    block = sbrk(size);
+    if (block == (void*)-1)
+        return NULL;
+    return block;
+}
