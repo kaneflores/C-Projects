fc0bc704b7a25d1c78edf24a7e58bf5ea6f4cf2e
kaneflores <kaneflores911@gmail.com>
2026-04-17 22:46:09 -0400

    fixes to starter fiel

--- beginnings-of-c.c
+++ beginnings-of-c.c
@@ -1,11 +1,8 @@
-typedef char ALIGN[16];
-header_t *head, *tail;
-union header {
-    struct {
-        size_t size;
-        unsigned is_free;
-        union header *next;
-    } s;
-    ALIGN stub;
-};
-typedef union header header_t;
+/* hello.c */
+#include <beginnings-of-c.c>
+
+int main(){
+    printf("Hello, World!\n");
+    
+    return 0;
+}
